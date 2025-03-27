57c8cf57c467418a96697c86e1efe1c775a3bdd2
Diegolinharesvenancio <diegolinharesvenancio@gmail.com>
2025-03-27 16:54:30 -0300

    Project created

---
+++ src/test/java/diegovenancio/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package diegovenancio.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
