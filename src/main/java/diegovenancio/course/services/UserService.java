d048098ea1b340b5f89c264c2d99950c59073712
Diegolinharesvenancio <diegolinharesvenancio@gmail.com>
2025-05-21 07:52:06 -0300

    User update

--- src/main/java/diegovenancio/course/services/UserService.java
+++ src/main/java/diegovenancio/course/services/UserService.java
@@ -30,5 +30,16 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User updata(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updataData(entity, obj);
+		return repository.save(entity);
+	}
+	private void updataData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 	}
 
