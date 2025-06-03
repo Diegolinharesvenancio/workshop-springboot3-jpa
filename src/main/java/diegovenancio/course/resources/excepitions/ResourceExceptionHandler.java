b9e2f3e2d47075402ae4f5689bbbdddf355661f6
Diegolinharesvenancio <diegolinharesvenancio@gmail.com>
2025-06-03 19:51:55 -0300

    Exception handling - delete

--- src/main/java/diegovenancio/course/resources/excepitions/ResourceExceptionHandler.java
+++ src/main/java/diegovenancio/course/resources/excepitions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import diegovenancio.course.services.exceptions.DatabaseException;
 import diegovenancio.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,10 +15,10 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFoun(ResourceNotFoundException e, HttpServletRequest request){
-		String error = "Resource not found";
-		HttpStatus status = HttpStatus.NOT_FOUND;
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/diegovenancio/course/services/UserService.java
+++ src/main/java/diegovenancio/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import diegovenancio.course.entites.User;
 import diegovenancio.course.repositories.UserRepository;
+import diegovenancio.course.services.exceptions.DatabaseException;
 import diegovenancio.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +31,14 @@ public class UserService {
 	}
 	
 	public void delete (Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			 throw new ResourceNotFoundException(id); 
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User updata(Long id, User obj) {
